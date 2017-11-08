d14c37c67099fce5039fba5d071e18eaaaa21573
Steven Neubacher <stevee19@live.nl>
2017-11-08 11:38:22 +0100

    Initial commit

---
+++ MqttPublisher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Net;
+
+namespace apiScraper
+{
+    class MqttPublisher
+    {
+        private MqttClient client;
+
+        public MqttPublisher()
+        {
+            // This should be updated by the actual hostname of the MQTT broker
+            client = new MqttClient("localhost");
+            string clientId = Guid.NewGuid().ToString();
+            try
+            {
+                client.Connect(clientId);
+                string[] topics = { "/weather", "/traffic" };
+                byte[] toBytes = Encoding.ASCII.GetBytes("Mooi weer");
+                client.Publish("/weather", toBytes);
+                client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Could not connect to MQTT Broker");
+            }
+
+        }
+
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            Console.WriteLine("MQTT Received");
+            Console.WriteLine("Topic: " + e.Topic);
+            Console.WriteLine("Message: " + e.Message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace apiScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MqttPublisher publisher = new MqttPublisher();
+        }
+    }
+}
